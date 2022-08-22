22badf2bd5740dfaf105b55dc96cb600b9bd65a8
LiaAiluri <0238410-github@lia.vc>
2022-08-22 21:26:34 +0200

    Added Image Recognition for URLs

--- src/services/file_recognition_service.rs
+++ src/services/file_recognition_service.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, HttpResponse, HttpRequest};
+use actix_web::{get, post, HttpResponse, HttpRequest, web};
 use actix_web::web::Bytes;
 use crate::helper::{misc, db_api_helper};
 use crate::{s3_helpers, web_helper};
@@ -112,6 +112,74 @@ pub async fn detect_image(req: HttpRequest, body: Bytes) -> HttpResponse {
     }
 }
 
+///API endpoint, that detects the type of the data given by the URL in it's body and returns the scan result, appropriate for the data type
+/// 
+/// # Arguments
+/// req: HttpRequest - The request object
+/// body: Bytes - The body of the request
+/// 
+/// # Returns
+/// HttpResponse - The response object
+#[post("scan/v1/detection/detectImageFromUrl")]
+pub async fn detect_img_from_url(req: HttpRequest, body: Bytes) -> HttpResponse {
+    if !web_helper::check_auth(&req).await{
+        return HttpResponse::Unauthorized().finish();
+    }
+
+    let image_url = String::from_utf8(body.to_vec());
+
+    if image_url.is_err(){
+        return HttpResponse::from(HttpResponse::BadRequest().body("Please ensure you specify a valid url to detect from"))
+    }
+
+    let unwrapped_url = image_url.unwrap();
+
+    if unwrapped_url.is_empty(){
+        return HttpResponse::from(HttpResponse::BadRequest().body("Please ensure you specify a valid url to detect from"))
+    }
+
+    let image_data = reqwest::get(unwrapped_url).await;
+
+    if image_data.is_err(){
+        return HttpResponse::from(HttpResponse::BadRequest().body("Could not fetch the image from the given url. Please make sure the url is correct."))
+    }
+
+    let image_byte_result = image_data.unwrap().bytes().await;
+    
+    if image_byte_result.is_err(){
+        return HttpResponse::from(HttpResponse::BadRequest().body("Error while trying to load the image as bytes. Please ensure the url is correct and we can get images from it."))
+    }
+
+    let image_bytes = image_byte_result.unwrap();
+
+    let result = get_image_recognition_result(&image_bytes).await;
+
+    if result.is_ok(){
+        return HttpResponse::Ok().body(result.unwrap());
+    }
+    else{
+        let result_code = result.err().unwrap();
+
+        if result_code.0 == 500{
+            return HttpResponse::InternalServerError().body(result_code.1);
+        }
+        if result_code.0 == 400{
+            return HttpResponse::BadRequest().body(result_code.1);
+        }
+        if result_code.0 == 301{
+            //We issue a 301 if the request takes too long to process but direct them to the same URL with a 60 second wait time
+
+            let moved_response =  
+                HttpResponse::Ok()
+                .append_header(("Retry-After", "60"))
+                .body(result_code.1);
+            return moved_response;
+        }
+
+        return HttpResponse::BadRequest().body("Could not process the request that has been sent to the server.".to_string());
+    }
+}
+
 ///Gets the scan result of the data, either from our database or from scanning the data via our scanning nodes
 /// # Arguments
 /// * `image` - The image to scan
@@ -127,7 +195,7 @@ pub async fn detect_image(req: HttpRequest, body: Bytes) -> HttpResponse {
 /// let result = get_image_recognition_result(Bytes::from(image)).await;
 /// ```
 async fn get_image_recognition_result(image: &Bytes) -> Result<String, (i16, String)>{
-    let resized_image = misc::resize_image(image, &450, &0).await;
+    let resized_image = misc::resize_image(image, &250, &250).await;
 
     if resized_image.is_none(){
         return Err((500, "Encountered an issue while attempting to process your image. Please validate it's data type is correct.".to_string()));
